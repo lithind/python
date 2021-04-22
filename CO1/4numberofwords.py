8ea15e071f47820a69c6d36ca8fc1d229e2df6b6
AswinP2711 <lithind64@gmail.com>
2021-04-23 00:22:13 +0530

    pgm 4

--- CO1/4numberofwords.py
+++ CO1/4numberofwords.py
@@ -1,3 +1,12 @@
 text=input("enter the sentence:")
 for i in text.split():
     print("Number of occurence of word ",i," is :",text.count(i))
+# s = input("enter line of text:")
+# count = {}
+# words = s.split()
+# for word in words:
+#   if word in count:
+#     count[word] += 1
+#   else:
+#     count[word] = 1
+# print(count)
